4f04866975f2632b712ff64ebf58e2bfd770f8f9
FranciscoJavier04 <fmarcor2304b@iesmarquesdecomares.org>
2025-02-10 22:50:14 +0100

    cambios

--- proyectoAngular/frontend/src/app/app.component.ts
+++ proyectoAngular/frontend/src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, RegistroComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Francisco Futbol';
 }
--- proyectoAngular/frontend/src/app/app.routes.ts
+++ proyectoAngular/frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Productos',
+        component: HomeComponent,
+      },
+      {
+    path: 'login',
+    title: 'Inicio Sesión',
+    component: LoginComponent,
+  },
+  {
+    path: 'registro',
+    title: 'Registro',
+    component: RegistroComponent,
+  }];
