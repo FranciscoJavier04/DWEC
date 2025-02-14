113bd8b8e499a1459da02cc4f6e27560846a274b
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2025-02-14 12:17:39 +0100

    navbar

--- proyectoAngular/frontend/src/app/app.component.ts
+++ proyectoAngular/frontend/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegistroComponent } from './components/registro/registro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, RegistroComponent, LoginComponent],
+  imports: [RouterOutlet, HomeComponent, RegistroComponent, LoginComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
