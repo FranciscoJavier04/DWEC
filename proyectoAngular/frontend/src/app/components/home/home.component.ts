d7528f0e7348cc1b2df7a075e6b222345c527a0f
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2025-02-17 14:11:24 +0100

    conexionSpringboot

--- proyectoAngular/frontend/src/app/components/home/home.component.ts
+++ proyectoAngular/frontend/src/app/components/home/home.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../interfaces/usuario';
+import { UsuarioService } from '../../services/usuario.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  usuarias: Usuario[] = [];
+  errorMessage: string = '';
+  constructor(private usuariaService: UsuarioService) {}
+  ngOnInit(): void {
+    this.obtenerUsuarias();
+  }
+  obtenerUsuarias(): void {
+    this.usuariaService.obtenerListaEmpleados().subscribe({
+      next: (data) => {
+        this.usuarias = data;
+      },
+      error: (err) => {
+        this.errorMessage = 'Error al obtener las usuarias';
+        console.error(err);
+      },
+    });
+  }
+}
---
+++ proyectoAngular/frontend/src/app/interfaces/usuario.ts
@@ -0,0 +1,11 @@
+export interface Usuario {
+  id: number;
+  email: string;
+  password: string;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  pais: string;
+  aficiones: string;
+  admin: boolean;
+}
