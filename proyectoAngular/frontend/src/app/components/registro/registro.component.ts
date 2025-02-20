d11c2f6cb5ed2cf791b46fa32ccc67753d915d0b
franciscojavier04 <fmarcor2304b@iesmarquesdecomares.org>
2025-02-20 10:31:50 +0100

    registro casi acabado

--- proyectoAngular/frontend/src/app/components/registro/registro.component.ts
+++ proyectoAngular/frontend/src/app/components/registro/registro.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { DatosAltaUsuario } from '../../interfaces/datosAltaUsuario';
 import { UsuarioService } from '../../services/usuario.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent {
+  registroForm: FormGroup;
   usuario: DatosAltaUsuario = {
     nombre: '',
     apellidos: '',
@@ -21,12 +29,31 @@ export class RegistroComponent {
     admin: 0,
   };
 
-  constructor(private usuarioService: UsuarioService) {}
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService) {
+    this.registroForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellidos: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      pais: ['', Validators.required],
+      sexo: ['', Validators.required],
+      aficiones: [''],
+    });
+  }
 
   registrar() {
-    this.usuarioService.anadirUsuario(this.usuario).subscribe({
-      next: () => alert('Usuario registrado con éxito'),
-      error: () => alert('Error en el registro'),
-    });
+    if (this.registroForm.valid) {
+      // Asignar los valores del formulario al objeto usuario
+      this.usuario = { ...this.registroForm.value };
+
+      // Llamar al servicio para agregar el usuario
+      this.usuarioService.anadirUsuario(this.usuario).subscribe({
+        next: () => alert('Usuario registrado con éxito'),
+        error: () => alert('Error en el registro'),
+      });
+    } else {
+      console.log('Formulario inválido');
+      this.registroForm.markAllAsTouched();
+    }
   }
 }
